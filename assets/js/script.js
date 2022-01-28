a512e795f6e3ba80166eba85c76da58ba54a9b69
becket <becket.bowes@gmail.com>
2022-01-27 23:31:02 -0500

    functional hardcoded request to ticketmaster

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
 var getEvents = function() {
-    fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=qMeZuZFNC7wTNBRfgRDNS9UVVganTX77", {mode: "cors"})
+    //latlong string 40.7138,-74.0060
+    //radius string
+    //startDateTime YYYY-MM-DD
+    //endDateTime YYYY-MM-DD
+    //city Array
+    //stateCode String
+    //classificationName Array
+    //size (# of responses) String
+    //genreId Array
+    //typeId Array
+    //geoPoint Geohash string
+    //sort date,name,desc
+
+    //var exampleLatLong = "latlong=40.7138,-74.0060"
+    //var exampleStartDateTime = "startDateTime=2022-02-22T00:00:01Z"
+    //var exampleEndDateTime = "endDateTime=2022-02-24T23:59:59Z"
+
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?latlong=40.7138,-74.0060&startDateTime=2022-02-22T00:00:01Z&endDateTime=2022-02-24T23:59:59Z&size=100&apikey=qMeZuZFNC7wTNBRfgRDNS9UVVganTX77")
     .then(function (response) {
         return response.json();
     })
@@ -32,34 +49,36 @@ getEvents();
 //if you search for a city on the index page you can see the console.log result in the inspector.
 //btw, you're welcome to use the google geocode api key and any of this code in other projects you may be working on:)
 
-//call google api and get lat/long or other geo identifier for city for ticketmaster to use
-// var getLocation = function(param) {
-//     var googleMayI = ("https://maps.googleapis.com/maps/api/geocode/json?address=" + param + "&key=AIzaSyDWtVKZCyc6X5L_eERu0Bk_WpclnefusjU");
-//     fetch(googleMayI)
-//         .then(function (response) {
-//             return response.json();
-//         })
-//         .then(function (data) {
-//             //error function 
-//             if (!data || data.status === 'ZERO_RESULTS') {
-//                 alert("Something went wrong, please enter a search with only three words to describe the place you're looking for.");
-//             }
-//             //format the data, baby
-//             console.log(data);
-//         });
-// };
+// call google api and get lat/long or other geo identifier for city for ticketmaster to use
 
-// //this button is to get user request and format it for google geocode service
-// document.getElementById("search-button").addEventListener("click", function () {
-//     var placeSearchName = document.getElementById("city-search-field").value;
-//     const words = placeSearchName.split(' ');
-//     if (words.length > 1) {
-//         const string = (words[0] + "_" + words[1]);
-//         getLocation(string);
-//     } else if (words.length > 2) {
-//         const string = (words[0] + "_" + words[1] + "_" + words[2]);
-//         getLocation(string);
-//     } else {
-//         getLocation(placeSearchName);
-//     };
-// });
+var getLocation = function(param) {
+    var googleMayI = ("https://maps.googleapis.com/maps/api/geocode/json?address=" + param + "&key=AIzaSyDWtVKZCyc6X5L_eERu0Bk_WpclnefusjU");
+    fetch(googleMayI)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            //error function 
+            if (!data || data.status === 'ZERO_RESULTS') {
+                alert("Something went wrong, please enter a search with only three words to describe the place you're looking for.");
+            }
+            //format the data, baby
+            console.log(data);
+        });
+};
+
+//this button is to get user request and format it for google geocode service
+
+document.getElementById("search-button").addEventListener("click", function () {
+    var placeSearchName = document.getElementById("city-search-field").value;
+    const words = placeSearchName.split(' ');
+    if (words.length > 1) {
+        const string = (words[0] + "_" + words[1]);
+        getLocation(string);
+    } else if (words.length > 2) {
+        const string = (words[0] + "_" + words[1] + "_" + words[2]);
+        getLocation(string);
+    } else {
+        getLocation(placeSearchName);
+    };
+});
