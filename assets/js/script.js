11ea7282ef01ac9f735dfa24665f3aaa692e6733
becket <becket.bowes@gmail.com>
2022-01-27 20:18:35 -0500

    updated java

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,14 @@
+var getEvents = function() {
+    fetch("https://app.ticketmaster.com/discovery/v2/events.json?classificationName=music&dmaId=324&apikey=qMeZuZFNC7wTNBRfgRDNS9UVVganTX77", {mode: "cors"})
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (data) {
+        console.log(data);
+    });
+};
+getEvents();
+
 //add eventlisteners to buttons to get citys
 //add calendar drop down thing from week 4(?) to get date
 //format calls to apis
@@ -22,33 +33,33 @@
 //btw, you're welcome to use the google geocode api key and any of this code in other projects you may be working on:)
 
 //call google api and get lat/long or other geo identifier for city for ticketmaster to use
-var getLocation = function(param) {
-    var googleMayI = ("https://maps.googleapis.com/maps/api/geocode/json?address=" + param + "&key=AIzaSyDWtVKZCyc6X5L_eERu0Bk_WpclnefusjU")
-    fetch(googleMayI)
-        .then(function (response) {
-            return response.json();
-        })
-        .then(function (data) {
-            //error function 
-            if (!data || data.status === 'ZERO_RESULTS') {
-                alert("Something went wrong, please enter a search with only three words to describe the place you're looking for.")
-            }
-            //format the data, baby
-            console.log(data);
-        });
-};
+// var getLocation = function(param) {
+//     var googleMayI = ("https://maps.googleapis.com/maps/api/geocode/json?address=" + param + "&key=AIzaSyDWtVKZCyc6X5L_eERu0Bk_WpclnefusjU");
+//     fetch(googleMayI)
+//         .then(function (response) {
+//             return response.json();
+//         })
+//         .then(function (data) {
+//             //error function 
+//             if (!data || data.status === 'ZERO_RESULTS') {
+//                 alert("Something went wrong, please enter a search with only three words to describe the place you're looking for.");
+//             }
+//             //format the data, baby
+//             console.log(data);
+//         });
+// };
 
-//this button is to get user request and format it for google geocode service
-document.getElementById("search-button").addEventListener("click", function () {
-    var placeSearchName = document.getElementById("city-search-field").value;
-    const words = placeSearchName.split(' ');
-    if (words.length > 1) {
-        const string = (words[0] + "_" + words[1]);
-        getLocation(string);
-    } else if (words.length > 2) {
-        const string = (words[0] + "_" + words[1] + "_" + words[2]);
-        getLocation(string);
-    } else {
-        getLocation(placeSearchName);
-    };
-});
+// //this button is to get user request and format it for google geocode service
+// document.getElementById("search-button").addEventListener("click", function () {
+//     var placeSearchName = document.getElementById("city-search-field").value;
+//     const words = placeSearchName.split(' ');
+//     if (words.length > 1) {
+//         const string = (words[0] + "_" + words[1]);
+//         getLocation(string);
+//     } else if (words.length > 2) {
+//         const string = (words[0] + "_" + words[1] + "_" + words[2]);
+//         getLocation(string);
+//     } else {
+//         getLocation(placeSearchName);
+//     };
+// });
