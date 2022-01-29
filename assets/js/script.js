09b5e490c6965884b3c049a9e06ef927008220ae
becket <becket.bowes@gmail.com>
2022-01-29 16:58:50 -0500

    fixed location search and returning dates inside the buttons

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,50 @@
-//TODO: fix location tool
-//TODO: make an if clause for eventprices that are free to prevent error in looping
 //TODO: replace startDateTime and endDateTime with input from User
 //TODO: replace classificationName with input from User
+//TODO: reformat eventDate and eventTime output 
+//TODO: save button
+//TODO: name button
 
 //get event list from TicketMaster
 var getEvents = function(latLong) {
     var startDateTime = ("2022-02-22T00:00:01Z");
     var endDateTime = ("2022-02-24T23:59:59Z");
-    var classificationName = "Sports";
-    var ticketMasterMayI = ("https://app.ticketmaster.com/discovery/v2/events.json?latlong=" + latLong + "&startDateTime=" + startDateTime + "&endDateTime=" + endDateTime + "&classificationName=" + classificationName + "&size=100&sort=date,name,desc&apikey=qMeZuZFNC7wTNBRfgRDNS9UVVganTX77");
+    var classificationName = "Music";
+    var ticketMasterMayI = ("https://app.ticketmaster.com/discovery/v2/events.json?latlong=" + latLong + "&radius=10&startDateTime=" + startDateTime + "&endDateTime=" + endDateTime + "&classificationName=" + classificationName + "&size=100&sort=date,name,desc&apikey=qMeZuZFNC7wTNBRfgRDNS9UVVganTX77");
     fetch(ticketMasterMayI)
     .then(function (response) {
         return response.json();
     })
     .then(function (data) {
         console.log(data);
-        for (i=0; 1<data._embedded.events.length; i++) {
+        for (i=0; 1<data.page.totalElements; i++) {
             var eventName = (data._embedded.events[i].name);
             var eventLocation = (data._embedded.events[i]._embedded.venues[0].name);
             var eventLink = (data._embedded.events[i].url);
             var eventImage = (data._embedded.events[i].images[0].url);
+            var eventDate = (data._embedded.events[i].dates.start.localDate);
+            var eventTime = (data._embedded.events[i].dates.start.localTime);
+            var eventDateTime = (eventDate + ", " + eventTime);
+            if (!!data._embedded.events[i].sales) {
+                eventPrice = "Tickets are not yet on sale";
+            } else if (data._embedded.events[i].priceRanges[0].max < 0){
             var eventPriceMin = (data._embedded.events[i].priceRanges[0].min);
             var eventPriceMax = (data._embedded.events[i].priceRanges[0].max);
             var eventPrice = ("Prices from $" + eventPriceMin + " - $" +eventPriceMax);
-            eventToken(eventName, eventLocation, eventLink, eventImage, eventPrice);
+            } else {
+                eventPrice = "Free";
+            };
+            eventToken(eventDateTime, eventName, eventLocation, eventLink, eventImage, eventPrice);
         };
     });
 };
 
 //make button from results or (eventually) from local storage
-var eventToken = function(eventName, eventLocation, eventLink, eventImage, eventPrice) {
+var eventToken = function(eventDateTime, eventName, eventLocation, eventLink, eventImage, eventPrice) {
     var resultsField = document.getElementById("results-buttons");
     var token = document.createElement("button");
     var tokenImage = document.createElement("img");
+    var tokenDiv = document.createElement("div");
+    var tokenDateTime = document.createElement("p");
     var tokenTitle = document.createElement("a");
     var tokenLocation = document.createElement("p");
     var tokenPrice = document.createElement("p");
@@ -40,14 +52,17 @@ var eventToken = function(eventName, eventLocation, eventLink, eventImage, event
     tokenImage.setAttribute("alt", "Event Image");
     //we can replace next line if we style everything; note - these images tend to have 16:9 ratio.
     tokenImage.setAttribute("style", "height:75px");
+    tokenDateTime.textContent = eventDateTime;
     tokenTitle.textContent = eventName;
     tokenTitle.setAttribute("href", eventLink);
     tokenLocation.textContent = eventLocation;
     tokenPrice.textContent = eventPrice;
     token.appendChild(tokenImage);
-    token.appendChild(tokenTitle);
-    token.appendChild(tokenLocation);
-    token.appendChild(tokenPrice);
+    token.appendChild(tokenDiv);
+    tokenDiv.appendChild(tokenDateTime);
+    tokenDiv.appendChild(tokenTitle);
+    tokenDiv.appendChild(tokenLocation);
+    tokenDiv.appendChild(tokenPrice);
     resultsField.appendChild(token);
 };
 
@@ -86,8 +101,10 @@ var getLocation = function(param) {
             if (!data || data.status === 'ZERO_RESULTS') {
                 alert("Something went wrong, please enter a search with only three words to describe the place you're looking for.");
             } else {
-                var lat = (data.results[0].geometry.location.lat);
-                var lng = (data.results[0].geometry.location.lng);
+                var lat = (data.results[0].geometry.location.lat).toPrecision(6);
+                var lng = (data.results[0].geometry.location.lng).toPrecision(6);
+                console.log("getLocation:" + lat);
+                console.log("getLocation:" + lng);
                 var latLong = (lat + "," + lng);
                 console.log("getLocation:" + latLong);
                 getEvents(latLong);
